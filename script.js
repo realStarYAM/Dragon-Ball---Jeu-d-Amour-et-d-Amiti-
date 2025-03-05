57a914606e6dea8c0d10a23a435b1d4f26592a3c
AdelWolf5 <AdelWolf5@outlook.fr>
2025-03-05 12:35:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,13 @@ document.addEventListener("DOMContentLoaded", function () {
 function startGame() {
     let character = document.getElementById("character").value;
     let country = document.getElementById("country").value;
-    
-    let resultText = `Tu pars en voyage avec ${character} dans ${country} !`;
+
+    let characterName = character.charAt(0).toUpperCase() + character.slice(1);
+    let resultText = `Tu pars en voyage avec ${characterName} dans ${country} !`;
     document.getElementById("result").textContent = resultText;
+
+    let imgSrc = `images/${character}.png`; // Exemple : images/vegeta.png
+    let charImg = document.getElementById("character-img");
+    charImg.src = imgSrc;
+    charImg.style.display = "block";
 }
